c0244be3944a2ef23d85399206e634a764d7a35a
mausooq <mauchoomausooq051@gmail.com>
2024-04-27 23:33:13 +0530

    recersive function method

--- binary_search.java
+++ binary_search.java
@@ -14,11 +14,26 @@ public class binary_search {
         }
         return -1;
     }
+    public static int binarySearch2(int[] num ,int target, int left ,int right ){
+        if(left <= right){
+            int mid =(left+right)/2;
+            if(num[mid] == target)
+                return mid;
+            else if(num[mid] < target)
+                return binarySearch2(num,target,mid+1,right);
+            else if(num[mid] > target)
+                return binarySearch2(num,target,left,mid-1);
+        }
+
+        return -1;
+
+    }
 
     public static void main(String[] args) {
         int[] num= {11,22,31,46,166,300,500};
-        int target =3;
-        int result = binarySearch(num ,target );
+        int target =166;
+//        int result = binarySearch(num ,target );
+        int result = binarySearch2(num ,target, 0, num.length-1 );
         if(result != -1)
             System.out.println("element fount at index " +result);
         else
