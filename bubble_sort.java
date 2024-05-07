34ff8f41bbe6f025f0e771bd26b207b113e4a4e6
mausooq <mauchoomausooq051@gmail.com>
2024-05-07 23:56:30 +0530

    bubble sort

--- bubble_sort.java
+++ bubble_sort.java
@@ -1,5 +1,5 @@
 public class bubble_sort {
-    public static int bubbleSort(int[] array){
+    public static void bubbleSort(int[] array){
         int i=0;
         while(i<array.length-1){
             for(int j=0;j<array.length-1;j++){
@@ -10,20 +10,21 @@ public class bubble_sort {
              }
             }
             i++;
-            System.out.println(" ");
-            for (int m = 0; m < array.length; m++) {
-                System.out.print(array[m] +" ");
-            }
         }
-        return -1;
     }
     public static void main(String[] args) {
-        bubble_sort b1 = new bubble_sort();
         int[] array = {6,8,9,2,4,5};
         System.out.print("before sorting array - ");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] +" ");
+        for (int k : array) {
+            System.out.print(k + " ");
         }
+
         bubbleSort(array);
+
+        System.out.println();
+        System.out.print("after sorting array - ");
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
     }
 }
