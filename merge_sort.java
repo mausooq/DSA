4dcd2271abee892eb5db660d7df17aaa11425900
mausooq <mauchoomausooq051@gmail.com>
2024-05-19 17:34:08 +0530

    mergeSort

--- merge_sort.java
+++ merge_sort.java
@@ -1,17 +1,45 @@
+import javax.naming.PartialResultException;
+
 public class merge_sort {
-    public static void mergeSort(int[] arr,int left, int right ){
-        if(left>right){
-            int mid =(left + right)/2;
-            mergeSort(arr,left,mid);
-            mergeSort(arr, left, mid+1);
-            merge(arr,left,mid,right);
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, left, mid + 1);
+            merge(arr, left, mid, right);
         }
     }
-    public static void merge(int[] arr, int left,int mid , int rigth ){
-            int[] newArr;
-            int i=0,j=0,k=0;
 
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int[] newArr = new int[arr.length];
+        int i = left, j = mid + 1, k = 0;
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                newArr[k] = arr[i];
+                k++;
+                i++;
+            } else {
+                newArr[k] = arr[j];
+                k++;
+                j++;
+            }
         }
+        while (i <= mid) {
+            newArr[k] = arr[i];
+            k++;
+            i++;
+        }
+
+        while (j <= right){
+            newArr[k] = arr[j];
+        k++;
+        j++;
+    }
+        for(int l = 0; l < arr.length; l++) {
+            arr[l] = newArr[l];
+        }
+
+}
     public static void main(String[] args) {
         int[] array = {5 ,6 , 2, 3 , 1};
         System.out.print("before sorting array - ");
